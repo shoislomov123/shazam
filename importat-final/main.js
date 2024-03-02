0e6c346313bfcc12a4ce108cdfda41cbadf43cd1
shoislomov123 <161194631+shoislomov123@users.noreply.github.com>
2024-03-02 13:05:04 +0500

    Add files via upload

---
+++ importat-final/main.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger')
+const links = document.querySelector('.header__wrap-links')
+     
+
+
+
+
+burger.addEventListener('click', () =>{
+    links.classList.toggle('window')
+})
